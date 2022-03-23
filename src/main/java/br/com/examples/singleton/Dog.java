245aa2fa04176e0cb16e159dc8248ff7008b2b4f
lucas felix bastos <felixbastos.lucas@gmail.com>
2022-03-23 19:46:06 -0300

    Singleton pattern added

---
+++ src/main/java/br/com/examples/singleton/Dog.java
@@ -0,0 +1,35 @@
+package br.com.examples.singleton;
+
+public class Dog {
+	
+	private String name;
+	private String color;
+	private static Dog instance = new Dog();
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	private Dog() {}
+	
+	public static Dog getInstance() {
+		if(instance==null)
+			instance = new Dog();
+		return instance;
+	}
+	
+	
+}
---
+++ src/main/java/br/com/examples/singleton/SingletonTest.java
@@ -0,0 +1,33 @@
+package br.com.examples.singleton;
+
+public class SingletonTest {
+ 
+	
+	
+	/* [EN]
+	 * 
+	 * This method test the Dog class, in which the singleton pattern is applied.
+	 * When we call the Dog.getInstance for dog2 it already has an instance (which is in dog1), so originally it's 
+	 * just one dog object instantiated in both variables. And because of that the value is changed in both variables when
+	 * the setName method is called.
+	 * 
+	 * [PT]
+	 * 
+	 * Neste método testamos a classe Dog, na qual o padrão singleton foi aplicado.
+	 * Quando nós invocamos o método Dog.getInstance para a variável dog2 o método já possui uma instância (que está
+	 * em dog1), portanto existe apenas um objeto dog instanciado nas duas variáveis. Por isso o valor é alterado nas 
+	 * duas variáveis ao invocarmos o método setName.
+	 * 	 
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		Dog dog1 = Dog.getInstance();
+		dog1.setName("rex");
+		
+		Dog dog2 = Dog.getInstance();
+		dog2.setName("carl");
+	
+		System.out.println("the first dog is called: "+dog1.getName()+", and the second one is called: "+dog2.getName());
+	}
+}
